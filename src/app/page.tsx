91de968146222455d1524bf1ce03427f9400c892
minyjae <jiradate_ora@cmu.ac.th>
2024-09-09 15:26:24 +0700

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ import {
   Title,
 } from "@mantine/core";
 
-import { Form, useForm, zodResolver } from "@mantine/form";
+import { useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
 
 const schema = z
@@ -35,14 +35,14 @@ const schema = z
       .min(3, { message: "Last name must have at least 3 characters" }),
     email: z.string().email({ message: "Invalid email format" }),
     plan: z.enum(["funrun", "mini", "half", "full"], {
-      errorMap: (issue, ctx) => ({ message: "Please select a plan" }),
+      errorMap: () => ({ message: "Please select a plan" }),
     }),
     gender: z.enum(["male", "female"], {
-      errorMap: (issue, ctx) => ({ message: "Please choose a gender" }),
+      errorMap: () => ({ message: "Please choose a gender" }),
     }),
     acceptTermsAndConds: z.literal(true, {
       // message: "You must accept terms and conditions",
-      errorMap: (issue, ctx) => ({
+      errorMap: () => ({
         message: "You must accept terms and conditions",
       }),
     }),
@@ -126,7 +126,7 @@ export default function Home() {
         <Space h="lg" />
 
         {/* add form */}
-        <form onSubmit={form.onSubmit((v) => alert("See you at CMU Marathon"))}>
+        <form onSubmit={form.onSubmit((values) => alert("See you at CMU Marathon"))}>
           <Stack gap="sm">
             <Group grow align="start">
               <TextInput
